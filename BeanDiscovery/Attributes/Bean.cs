f50bd79bec2af0cc951f700b863ea4840bbc6c0c
José Espinoza <espinozasalas.jose@gmail.com>
2020-03-02 20:52:23 -0300

    Base Code

---
+++ BeanDiscovery/Attributes/Bean.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BeanDiscovery.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Bean : Attribute
+    {
+        public string Name { get; set; }
+        public ScopeType Scope { get; }
+
+        public Bean(string name = "Primary", ScopeType scope = ScopeType.TRANSIENT) 
+        {
+            Name = name;
+            Scope = scope;
+        }
+
+    }
+}
---
+++ BeanDiscovery/ScopeType.cs
@@ -0,0 +1,9 @@
+﻿namespace BeanDiscovery
+{
+    public enum ScopeType
+    {
+        TRANSIENT = 1,
+        SCOPED = 2,
+        SINGLETON = 3
+    }
+}
