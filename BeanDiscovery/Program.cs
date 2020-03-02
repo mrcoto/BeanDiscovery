50f30d78672ca7dca2abbe4cfadf79c2542ba3f9
José Espinoza <espinozasalas.jose@gmail.com>
2020-03-02 20:39:20 -0300

    Big Bang

---
+++ BeanDiscovery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeanDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
