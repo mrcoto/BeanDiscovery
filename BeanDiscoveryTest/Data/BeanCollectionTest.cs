b6a8258c455705d351ca4af70c9a580cad42b34c
José Espinoza <espinozasalas.jose@gmail.com>
2020-03-07 12:45:50 -0300

    Added BeanCollectionTest

--- BeanDiscoveryTest/Data/BeanCollectionTest.cs
+++ BeanDiscoveryTest/Data/BeanCollectionTest.cs
@@ -1,4 +1,5 @@
-﻿using BeanDiscovery.Data;
+﻿using BeanDiscovery.Config.Data;
+using BeanDiscovery.Data;
 using BeanDiscovery.Data.Exceptions;
 using BeanDiscoveryTest.BeanAttribute.Factory;
 using Xunit;
@@ -14,6 +15,29 @@ namespace BeanDiscoveryTest.Data
             var beanCollection = new BeanCollection(typeof(ILangBean));
             beanCollection.AddBean(typeof(SpanishLangBean));
             Assert.Throws<BeanPresentException>(() => beanCollection.AddBean(typeof(Spanish2LangBean)));
+        }
+
+        [Theory]
+        [InlineData("English", true, true)]
+        [InlineData("English", false, false)]
+        public void Test_Should_Throw_NotFoundBeanException(string beanName, bool throwExceptionIfNotFound, bool addSpanishLangBean)
+        {
+            var beanCollection = new BeanCollection(typeof(ILangBean));
+            if (addSpanishLangBean)
+                beanCollection.AddBean(typeof(SpanishLangBean));
+            var beanConfig = new BeanConfig(beanName, throwExceptionIfNotFound);
+            Assert.Throws<NotFoundBeanException>(() => beanCollection.FindBean(beanConfig));
+        }
+
+        [Fact]
+        public void Test_Should_FindBean()
+        {
+            var beanCollection = new BeanCollection(typeof(ILangBean));
+            beanCollection.AddBean(typeof(SpanishLangBean));
+            var beanConfig = new BeanConfig("Spanish", true);
+            var beanData = beanCollection.FindBean(beanConfig);
+            Assert.NotNull(beanData);
+            Assert.Equal(typeof(SpanishLangBean), beanData.TBean);
         }
 
     }
