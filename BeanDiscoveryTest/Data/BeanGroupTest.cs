cb55bce79b337f7ece66873336f8f283f97b04f0
José Espinoza <espinozasalas.jose@gmail.com>
2020-03-07 12:52:59 -0300

    Added bean group test

---
+++ BeanDiscoveryTest/Data/BeanGroupTest.cs
@@ -0,0 +1,31 @@
+﻿using BeanDiscovery.Config.Data;
+using BeanDiscovery.Data;
+using BeanDiscoveryTest.BeanAttribute.Factory;
+using System.Linq;
+using Xunit;
+
+namespace BeanDiscoveryTest.Data
+{
+    public class BeanGroupTest
+    {
+        [Fact]
+        public void Test_Should_Add_SingleBean()
+        {
+            var beanGroup = new BeanGroup();
+            beanGroup.Add(typeof(SingleBean));
+            Assert.Contains(beanGroup.SingleBeans, x => x.TBean == typeof(SingleBean));
+        }
+
+        [Fact]
+        public void Test_Should_Add_InterfaceWithBean()
+        {
+            var beanGroup = new BeanGroup();
+            beanGroup.Add(typeof(ILangBean), typeof(SpanishLangBean));
+            var beanCollection = beanGroup.InterfaceBeans.FirstOrDefault(x => x.TInterface == typeof(ILangBean));
+            Assert.NotNull(beanCollection);
+            var beanData = beanCollection.FindBean(new BeanConfig("Spanish", false));
+            Assert.NotNull(beanData);
+            Assert.Equal(typeof(SpanishLangBean), beanData.TBean);
+        }
+    }
+}
