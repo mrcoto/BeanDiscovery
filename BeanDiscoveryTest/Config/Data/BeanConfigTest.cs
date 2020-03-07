46426fa04c7f6666dcb751bfe994819269a9e007
José Espinoza <espinozasalas.jose@gmail.com>
2020-03-07 12:57:15 -0300

    BeanConfigTest

---
+++ BeanDiscoveryTest/Config/Data/BeanConfigTest.cs
@@ -0,0 +1,28 @@
+﻿using BeanDiscovery.Config.Data;
+using BeanDiscovery.Data.Exceptions;
+using Xunit;
+
+namespace BeanDiscoveryTest.Config.Data
+{
+    public class BeanConfigTest
+    {
+        [Theory]
+        [InlineData("Secondary", false)]
+        [InlineData("English", true)]
+        public void Test_Should_Set_Correct_Values(string name, bool throwIfNotFound)
+        {
+            var beanConfig = new BeanConfig(name, throwIfNotFound);
+            Assert.Equal(name, beanConfig.BeanName);
+            Assert.Equal(throwIfNotFound, beanConfig.ThrowExceptionIfNotFound);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Test_Should_Throw_EmptyBeanNameException(string name)
+        {
+            Assert.Throws<EmptyBeanNameException>(() => new BeanConfig(name));
+        }
+    }
+}
